e2caef621143dabcb873594c1baa5b7dc3089f4f
nmathew222 <nikhilkmathew22@gmail.com>
2022-05-17 16:33:25 +0100

    used get to reciever article id and error handling complete with tests.

---
+++ controller/articles.controller.js
@@ -0,0 +1,14 @@
+const {
+    fetchArticlesById
+}= require("../model/articles.model")
+
+exports.getArticlesById= (req,res,next)=>{
+    const {article_id}= req.params;
+    fetchArticlesById(article_id).then((article)=>{
+        res.status(200).send({article})
+    }).catch((err)=>{
+        next(err)
+
+
+    })
+}
