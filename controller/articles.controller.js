2576373158f0b181503414668416b9360dd27aad
nmathew222 <nikhilkmathew22@gmail.com>
2022-05-19 09:50:49 +0100

    refactered fetchArticlesById to recieve comment count based on id, 7 complete

--- controller/articles.controller.js
+++ controller/articles.controller.js
@@ -5,12 +5,10 @@ const {
 }= require("../model/articles.model")
 
 exports.getArticlesById= (req,res,next)=>{
-    console.log("inside controlerrrrrrr");
     const {article_id}= req.params;
     fetchArticlesById(article_id).then((article)=>{
         res.status(200).send({article})
     }).catch((err)=>{
-        console.log(err);
         next(err)
 
 
